3533761af36b908cd6f510921e42fcb8a5acde53
Nick Rakochy <nick.rakochy@gmail.com>
2026-04-14 09:49:05 -0500

    raython enumeration

--- crates/scanner/src/lib.rs
+++ crates/scanner/src/lib.rs
@@ -1,20 +1,44 @@
 use error::Error;
+use rayon::prelude::*;
+use reqwest::blocking::Client;
+use reqwest::redirect;
+use std::time::Duration;
 
 mod constants;
 mod model;
+mod ports;
 mod subdomains;
 
-pub fn scan(_target: &str) -> Result<(), error::Error> {
+pub fn scan(target: &str) -> Result<(), error::Error> {
+    let http_timeout = Duration::from_secs(5);
+    let http_client = Client::builder()
+        .redirect(redirect::Policy::limited(4))
+        .timeout(http_timeout)
+        .build()?;
+
+    // we use a custom threadpool to improve speed
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(256)
+        .build()
+        .unwrap();
+
+    // pool.install is required to use our custom threadpool, instead of rayon's default one
+    pool.install(|| {
+        let scan_result: Vec<model::Subdomain> = subdomains::enumerate(&http_client, target)
+            .unwrap()
+            .into_par_iter()
+            .map(ports::scan_ports)
+            .collect();
+
+        for subdomain in scan_result {
+            println!("{}:", &subdomain.domain);
+            for port in &subdomain.open_ports {
+                println!("    {}", port.port);
+            }
+
+            println!();
+        }
+    });
+
     Ok(())
 }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
--- crates/scanner/src/subdomains.rs
+++ crates/scanner/src/subdomains.rs
@@ -9,9 +9,11 @@ use trust_dns_resolver::{
     config::{ResolverConfig, ResolverOpts},
 };
 
+const URL: &str = "https://crt.sh";
+
 pub fn enumerate(http_client: &Client, target: &str) -> Result<Vec<Subdomain>, Error> {
     let entries: Vec<CrtShEntry> = http_client
-        .get(&format!("https://crt.sh/?q=%25.{}&output=json", target))
+        .get(format!("{}?q={}&output=json", URL, target))
         .send()?
         .json()?;
 
